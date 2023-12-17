674b265626a75ff1ebbdfbe1944de2a0c01c39e6
alinealien <alinebdantunes@gmail.com>
2023-12-17 12:17:48 -0300

    Atualizando JS do Projeto

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,39 +1,50 @@
-// Para trabalhar com o JS nesse projeto, vamos pensar nas funções que iremos precisar:
-// No Digimon Search, iremos precisar criar 3 funções simples, para:
+// Para trabalhar com o JS nesse projeto, iremos precisar criar 3 funções simples, para:
 // Chamar a API dos Digimons | Um filtro para busca dos Digimons | E uma função main.
 
 
-
-// Chamada da API:
+// CHAMADA DA API:
 //Função assincrona - async:
 async function getDigimonsAPI(){
 
 // Estamos criando uma variavel de nome 'response' para guardar a resposta da requisição API;
+// O fetch faz a requisição assincrona e o await irá pedir para aguardar a resposta;
 const response = await fetch("https://digitalinnovationone.github.io/api-digimon/api/digimon.json");
 
-//Aqui precisamos retornar as informações como um .json();
+// Aqui estamos retornando e convertendo a resposta para o JSON;
+// Novamente o await é utilizado para evitar a 'Promise';
 return await response.json();
 
 }
 
-//Filtro pra a busca Digimon:
+//FILTRO PARA A BUSCA DO DIGIMON:
 //Criar um digimonsList e digimonsId
 async function filtroDigimons(digimonsList, digimonId){
 
 //Armazenamos em 'digimon' o resultado de busca da filtragem:
     const digimon = await digimonsList.find((monster) => monster.id === digimonId);
 
+// Manipulei o DOM aqui para exibir as informações de nome e imagem dos Digimons:
+//É importante sempre observar a API e como está definidio suas propriedades.
+    const nomeDigimonElement = document.getElementById("t-nome__bt");
+    nomeDigimonElement.textContent = digimon.name;
+
+// Da mesma forma que busquei o nome do digimon, estou buscando a imagem, usando o querySelector.
+// NÃO ESQUECER DE COLOCAR O (.) ao atribuir o nome da classe da imagem 
+    const imgDigimonElement = document.querySelector(".i-card__digimon img");
+    imgDigimonElement.src = digimon.image;
+
+
     console.log(digimon);
 }
 
-//Função Main:
+// FUNÇÃO MAIN:
 async function main(){
 
     //1. Preciso chamar a API dos digimons primeiro:
     const digimons = await getDigimonsAPI();
 
     //2. Preciso chamar/filtrar o digimons escolhido:
-    const chooseDigimons = await filtroDigimons(digimons, 1);
+    const chooseDigimons = await filtroDigimons(digimons, 9);
 
 }
 
